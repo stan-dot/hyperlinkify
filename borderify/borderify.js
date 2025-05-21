513c92b34d7e1ff518f279dd8b4ac83636b748c3
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2025-05-21 13:28:42 +0100

    context aware

--- borderify/borderify.js
+++ borderify/borderify.js
@@ -6,9 +6,26 @@ const source = document.querySelector("div.moz24-newsletter-wrapper");
 source.addEventListener("copy", (event) => {
     const selection = document.getSelection();
     const selectedText = selection.toString();
-    const websiteUrl = window.location.href; // You can customize this if needed
 
-    const markdownLink = `[${selectedText}](${websiteUrl})`;
+    let anchorHref = null;
+    let node = selection.anchorNode;
+
+    // Traverse upwards to find a local anchor <a href="#...">
+    while (node && node !== document.body) {
+        if (node.nodeType === Node.ELEMENT_NODE) {
+            const anchor = node.querySelector?.('a[href^="#"]');
+            if (anchor) {
+                anchorHref = anchor.getAttribute("href");
+                break;
+            }
+        }
+        node = node.parentNode;
+    }
+
+    const baseUrl = window.location.origin + window.location.pathname;
+    const link = anchorHref ? `${baseUrl}${anchorHref}` : window.location.href;
+
+    const markdownLink = `[${selectedText}](${link})`;
 
     event.clipboardData.setData("text/plain", markdownLink);
     event.preventDefault();
