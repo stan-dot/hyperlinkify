f1d0caade13801e953c15c5f0192bb2aeb62ec97
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2025-05-21 13:26:26 +0100

    add the webiste link fast

--- borderify/borderify.js
+++ borderify/borderify.js
@@ -5,6 +5,11 @@ const source = document.querySelector("div.moz24-newsletter-wrapper");
 
 source.addEventListener("copy", (event) => {
     const selection = document.getSelection();
-    event.clipboardData.setData("text/plain", selection.toString().toUpperCase());
+    const selectedText = selection.toString();
+    const websiteUrl = window.location.href; // You can customize this if needed
+
+    const markdownLink = `[${selectedText}](${websiteUrl})`;
+
+    event.clipboardData.setData("text/plain", markdownLink);
     event.preventDefault();
 });
